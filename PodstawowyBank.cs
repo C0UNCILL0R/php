4d046a6dcc4540dc3d4eaa9c38c3c45cc20c17f1
C0UNCILL0R <c0uncill0r1000@gmail.com>
2025-09-19 11:23:11 +0200

    Bank Podstawowy - Programowanie Obiektowe S21

---
+++ PodstawowyBank.cs
@@ -0,0 +1,40 @@
+﻿namespace totr
+{
+    internal class Program
+    {
+        class Konto
+        {
+            public string Numer { get; }
+            public decimal Stan { get; set; }
+            public Konto() { }
+            public Konto(string numer, decimal stan)
+            {
+                Numer = numer;
+                Stan = stan;
+            }
+            public override string ToString()
+            {
+                return Numer + " " + Stan;
+            }
+            public void Przelej(Konto k, decimal kwota)
+            {
+                Stan -= kwota;
+                k.Stan += kwota;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Konto k1 = new Konto("2396239529587623874123", 2354.30m);
+            Console.WriteLine("Konto 1 : " + k1.ToString());
+            Konto k2 = new Konto("2389757897389605031356", 1238.30m);
+            Console.WriteLine("Konto 2 : " + k2.ToString());
+            k1.Stan = k1.Stan - 1000;
+            k2.Stan = k2.Stan + 1000;
+            Console.WriteLine("Konto 1 : " + k1.ToString());
+            Console.WriteLine("Konto 2 : " + k2.ToString());
+            k1.Przelej(k2, 300);
+            Console.WriteLine("Konto 1 : " + k1.ToString());
+            Console.WriteLine("Konto 2 : " + k2.ToString());
+        }
+    }
+}
